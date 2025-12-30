ca286a5f802541e11120c9d556d49710674fce4f
Vinny <vincentni2007@gmail.com>
2025-12-30 00:40:54 -0500

    wordle bot!

---
+++ bot.py
@@ -0,0 +1,113 @@
+import wordle
+game = wordle.Game()
+
+def new_counter():
+    return {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0,
+    }
+
+class Bot:
+    def __init__(self):
+        self.all_guesses = game.wordle_words
+        self.states = []
+        self.frequencies = []
+
+        for i in range(5):
+            counter = new_counter() 
+            for word in self.all_guesses:
+                counter[word[i]] += 1
+
+            self.frequencies.append(counter)
+
+    def calc_score(self, word):
+        sum = 0
+        for (c, f) in zip(list(word), self.frequencies):
+            sum += f[c]
+
+        return sum
+
+    def valid_word(self, word, greens, yellows, greys):
+        for c, i in greens:
+            if c != word[i]:
+                return False
+
+        for c in yellows:
+            if c not in word:
+                return False
+
+        for c in greys:
+            if c in word:
+                return False
+
+        return True
+
+    def remove_invalid(self):
+        greens = set([])
+        yellows = set([])
+        greys = set([])
+
+        for guess in self.states:
+            for i, state in enumerate(guess):
+                if state[1] == wordle.State.Green:
+                    greens.add((state[0], i))
+                elif state[1] == wordle.State.Yellow:
+                    yellows.add(state[0])
+                else:
+                    greys.add(state[0])
+
+        greens = list(greens)
+        yellows = list(yellows)
+        greys = list(greys)
+
+        for word in list(self.all_guesses):
+            if not self.valid_word(word, greens, yellows, greys):
+                self.all_guesses.remove(word)
+                continue
+
+    def get_best(self):
+        best = ""
+        high = 0
+        for word in self.all_guesses:
+            score = self.calc_score(word)
+            if score > high:
+                best = word
+                high = score
+
+        return best
+
+    def solve(self):
+        guess = ""
+
+        while guess is not game.answer:
+            guess = self.get_best()
+            state = game.get_state(guess)
+            game.print_state(state)
+            self.states.append(state)
+            self.remove_invalid()
+
+Bot().solve()
---
+++ wordle.py
@@ -0,0 +1,77 @@
+import random
+import enum
+
+class State(enum.Enum):
+    Green = 1
+    Yellow = 2
+    Grey = 3
+
+colors = {
+	State.Green: "\033[0;30m\033[42m",
+	State.Yellow: "\033[0;30m\033[43m",
+    State.Grey: "\033[0;30m\033[47m",
+    "None": "\033[0m",
+}
+
+class Game:
+    def __init__(self):
+        self.wordle_words = []
+        self.nguesses = 0
+        self.won = False
+        with open("answers.txt", "r") as f:
+            for line in f:
+                self.wordle_words.append(line.strip())
+
+        self.generate_answer()
+
+
+    def generate_answer(self):
+        self.answer = self.wordle_words[random.randint(0, len(self.wordle_words) - 1)]
+
+    def get_input(self):
+        while True:
+            guess = input("")
+            if len(guess) != 5:
+                print("must be length five.")
+                continue
+
+            for c in guess:
+                if c.isalpha() is False:
+                    print("only alphabetic characters.")
+                    continue
+
+            return guess
+
+    def get_state(self, guess):
+        out = []
+        for (a, c) in zip(list(self.answer), list(guess)):
+            state = State.Grey
+            if c is a:
+                state = State.Green
+            elif c in self.answer:
+                state = State.Yellow
+            
+            out.append((c, state))
+
+        return out
+
+    def print_state(self, state):
+        for (char, color) in state:
+            print(colors[color] + char + colors["None"], end="")
+
+        print("")
+
+    def run(self):
+        while True:
+            guess = self.get_input()
+            print("\033[1A\033[K", end="")
+            self.nguesses += 1
+            self.print_state(self.get_state(guess))
+
+            if guess == self.answer:
+                break;
+            elif self.nguesses >= 6:
+                break;
+
+# game = Game();
+# game.run()
